c9a0d1d6c1ef6ccc9219032f196fbb2d5cadaabb
tim977 <39566561+tim977@users.noreply.github.com>
2021-07-09 10:07:04 +0300

    all page

--- components/menu-modal/menu-modal.js
+++ components/menu-modal/menu-modal.js
@@ -1,3 +1,4 @@
+const body = document.getElementsByTagName('body')[0];
 const iconBurgerMenu = document.getElementsByClassName(
     'header__icon-burgerMenu',
 )[0];
@@ -6,8 +7,10 @@ const menuModal = document.getElementsByClassName('menu-modal')[0];
 
 iconBurgerMenu.addEventListener('click', () => {
     menuModal.setAttribute('data-open', '');
+    body.setAttribute('data-open', '');
 });
 
 iconClose.addEventListener('click', () => {
     menuModal.removeAttribute('data-open', '');
+    body.removeAttribute('data-open', '');
 });
